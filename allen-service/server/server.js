b7f331947230d77cd19ae92e40469e441e12f41f
allen-bui <allen.vbui@gmail.com>
2019-06-09 15:17:38 -0700

    [chore] adds jest and enzyme testing for components

--- allen-service/server/server.js
+++ allen-service/server/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const dbConnection = require('./../db/db.js');
 const app = express();
 const port = process.env.port || 3000;
 
+app.use(cors());
 app.use(express.static(__dirname + './../dist'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
