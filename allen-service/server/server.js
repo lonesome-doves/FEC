5675788acbd96c45e9e0f232c19f4bc285b66da4
unvee7 <harrison.unverferth@gmail.com>
2019-06-10 16:43:34 -0400

    tests initiated, needs work, deleted components/test.js and started to work

--- allen-service/server/server.js
+++ allen-service/server/server.js
@@ -2,14 +2,24 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const dbConnection = require('./../db/db.js');
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.port || 3001;
+const cors = require('cors');
 
+// const defaultCorsHeaders = {
+//   'access-control-allow-origin': '*',
+//   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+//   'access-control-allow-headers': 'content-type, accept',
+//   'access-control-max-age': 10,
+//   'content-type': 'application/json'
+// };
+
+app.use(cors());
 app.use(express.static(__dirname + './../dist'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/retrieve', (req, res) => {
-
+  // res.set(defaultCorsHeaders);
   dbConnection.retrieve((data) => {
     res.send(data);
   })
